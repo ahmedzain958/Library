197988d22487bac17bc1a006b7e3ae034692a6ed
ahmedzain958 <ahmedzain958@gmail.com>
2020-03-11 16:53:06 +0200

    msg queue handler looper

---
+++ app/src/main/java/com/zainco/library/TestingJava.java
@@ -0,0 +1,7 @@
+package com.zainco.library;
+
+public class TestingJava {
+    public static void main(String[] args) {
+        System.out.println("shmed.".replace(".","_"));
+    }
+}
--- app/src/main/java/com/zainco/library/threading/HandlerActivity.java
+++ app/src/main/java/com/zainco/library/threading/HandlerActivity.java
@@ -2,6 +2,7 @@ package com.zainco.library.threading;
 
 import android.os.Bundle;
 import android.os.Handler;
+import android.os.Looper;
 import android.view.View;
 import android.widget.Button;
 
@@ -13,12 +14,12 @@ public class HandlerActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
 
     private Button buttonStartThread;
+
     /*responsibility of handler is getting the work in the message queue
-    and works with the thread inwhich it has been instantiated in here for ex: it will work with the main thread
+    and works with the thread in which it has been instantiated in here for ex: ( main thread)
     which means we can use it to get our work to the main thread
     * */
-
-    private Handler handler = new Handler();
+    private Handler mainHandler = new Handler();
 /*
 every click on button or switch is added to the message queue and executed sequentially
 the Looper loops through the message queue
@@ -43,8 +44,46 @@ the Looper loops through the message queue
                 // android.view.ViewRootImpl$CalledFromWrongThreadException: Only the original thread that created a view hierarchy can touch its views.
                 //this means that ui widgets aren't thread safe; we can't access them from another thread
                 //be to access views from the background thread we need Handler
+                // method(1) handler
+                Handler handler = new Handler(Looper.getMainLooper());
+                /*this line will lead to crash in
+                handlerWithoutLooper.post(new Runnable() {
+                 can't create handler inside thread that hasn't called Looper.prepare() because the handler can only work iff we didn't pass a constructor to the handler*/
+
+
                 if (i == 5) {
-                    buttonStartThread.setText("asssssssssssssssssssssssssssssssssssssssssssssssasasas");
+                    handler.post(new Runnable() {
+
+                        /*for the thread that only has a looper and a msg queue(here main thread)*/
+//                    mainHandler.post(new Runnable() {//runnable itself doesn't start a new thread instead runnable is a package of work to be done
+                        @Override
+                        public void run() {
+                            buttonStartThread.setText("asssssssssssssssssssssssssssssssssssssssssssssssasasas");
+                        }
+                    });
+                    /////////////////////////////////////
+                                   /*
+                  method(2) post on button
+                  instead of using the upper looper we can also make
+                   * */
+                    buttonStartThread.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            buttonStartThread.setText("asssssssssssssssssssssssssssssssssssssssssssssssasasas");
+                        }
+                    });
+                    /////////////////////////////////////
+    /*
+                  method(3) use runOnUIThread()
+                                 * */
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            buttonStartThread.setText("asssssssssssssssssssssssssssssssssssssssssssssssasasas");
+                        }
+                    });
+
+
                 }
                 try {
                     Thread.sleep(1000);
